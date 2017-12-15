497f367b87e74cd401afae1c09fd0c4b57812c1c
Dani Schuhman <dani8439@aol.com>
2017-12-15 22:13:03 +0000

    details

--- lib/NYC_best/cli.rb
+++ lib/NYC_best/cli.rb
@@ -2,24 +2,20 @@ class NYCBest::CLI
 
   def call
     puts "Welcome to NYC Best Pizza!"
-    puts "----------------------------"
+    puts "--------------------------"
     sleep(1)
     # list_cuisines
     list_restaurants
+    puts ""
     menu
     goodbye
   end
 
   def list_restaurants
-    # puts "1. Juliana's - Dumbo - $$ - 4.7 stars"
-    # puts "2. Emily - Brooklyn - Clinton Hill - $$ - 4.7 stars"
-    # puts "3. Lucali - Carroll Gardens - $$ - 4.7 stars"
-    # puts "4. San Matteo Pizza Espresso Bar - East 80s - $$ - 4.6 stars"
-    # puts "5. Roberta's - Bushwick - $$ - 4.6 stars"
-    # puts "6. Paulie Gee's - Greenpoint - $$ - 4.6 stars"
-    # puts "7. DI Fara Pizza - Midwood - $$ - 4.6 stars"
-    # puts "8. Rubirosa Ristorante - NoLita - $$ - 4.5 stars"
     @restaurants = NYCBest::Restaurant.list
+    @restaurants.each.with_index(1) do |restaurant, i|
+      puts "#{i}. #{restaurant.name} - #{restaurant.location} - #{restaurant.price} - #{restaurant.rating}"
+    end
   end
 
   def menu
@@ -27,24 +23,19 @@ class NYCBest::CLI
     while input != "exit"
       puts "Please enter the number of the restaurant you are interested in, or type list to see choices, or type exit."
       input = gets.strip.downcase
-      case input
-      when "1"
-        puts "More info on Juliana's"
-      when "2"
-        puts "More info on Emily's"
-      when "3"
-        puts "More info on Lucali"
-      when "4"
-        puts "More info on San Matteo Pizza Espresso Bar"
-      when "5"
-        puts "More info on Roberta's"
-      when "6"
-        puts "More info on Paulie Gee's"
-      when "7"
-        puts "More info on DI Fara Pizza"
-      when "8"
-        puts "More info on Rubirosa Ristorante "
-      when "list"
+
+      if input.to_i > 0
+        the_restaurant = @restaurants[input.to_i-1]
+        puts ""
+        puts "#{the_restaurant.name} - #{the_restaurant.location} - #{the_restaurant.price} - #{the_restaurant.rating}"
+        puts "____________________________________________________________________________"
+        puts "#{the_restaurant.review}"
+        puts "____________________________________________________________________________"
+        puts "#{the_restaurant.recommended_dishes}"
+        puts "____________________________________________________________________________"
+        puts "#{the_restaurant.insider_tip}"
+        puts ""
+      elsif input == "list"
         list_restaurants
       else
         puts "Not sure what you want, type list or exit."
@@ -53,36 +44,36 @@ class NYCBest::CLI
   end
 
 
-  # def list_cuisines
-  #   cuisines = ["Brooklyn Pizza Delivery", "Manhattan Pizza Delivery", "NYC Pizza"]
-  #   # cuisines = ["Brooklyn Pizza", "Manhattan Pizza", "Steakhouse", "Indian", "Italian", "Mexican", "Seafood", "French", "Thai", "Barbecue", "Burger", "NYC Pizza", "Chinese", "American"]
-  #   cuisines.each.with_index(1) do |style, i|
-  #     puts "#{i}. #{style}"
-  #   end
-  # end
-  #
-  # def menu
-  #   input = nil
-  #   while input != "exit"
-  #     puts "Please enter the number of the pizza you would like more info on, or type list to see choices again, or type exit:"
-  #     input = gets.strip.downcase
-  #     case input
-  #     when "1"
-  #       puts "(brooklyn_restaurants)"
-  #     when "2"
-  #       puts "(manhattan_restaurants)"
-  #     when "3"
-  #       puts "(nyc_restaurants)"
-  #     when "list"
-  #       list_cuisines
-  #     else
-  #       puts "Not sure what you want, type list or exit."
-  #     end
-  #   end
-  # end
-
   def goodbye
     puts "See you when you're hungry!"
   end
 
 end
+
+# def list_cuisines
+#   cuisines = ["Brooklyn Pizza Delivery", "Manhattan Pizza Delivery", "NYC Pizza"]
+#   # cuisines = ["Brooklyn Pizza", "Manhattan Pizza", "Steakhouse", "Indian", "Italian", "Mexican", "Seafood", "French", "Thai", "Barbecue", "Burger", "NYC Pizza", "Chinese", "American"]
+#   cuisines.each.with_index(1) do |style, i|
+#     puts "#{i}. #{style}"
+#   end
+# end
+#
+# def menu
+#   input = nil
+#   while input != "exit"
+#     puts "Please enter the number of the pizza you would like more info on, or type list to see choices again, or type exit:"
+#     input = gets.strip.downcase
+#     case input
+#     when "1"
+#       puts "(brooklyn_restaurants)"
+#     when "2"
+#       puts "(manhattan_restaurants)"
+#     when "3"
+#       puts "(nyc_restaurants)"
+#     when "list"
+#       list_cuisines
+#     else
+#       puts "Not sure what you want, type list or exit."
+#     end
+#   end
+# end
--- lib/NYC_best/restaurant.rb
+++ lib/NYC_best/restaurant.rb
@@ -1,5 +1,5 @@
 class NYCBest::Restaurant
-  attr_accessor :name, :location, :price, :rating, :review, :recommended_dishes, :url
+  attr_accessor :name, :location, :price, :rating, :review, :recommended_dishes, :url, :insider_tip
 
   def self.list
     restaurant_1 = self.new
@@ -7,48 +7,77 @@ class NYCBest::Restaurant
     restaurant_1.location = "Dumbo"
     restaurant_1.price = "$$"
     restaurant_1.rating = "4.7 stars"
-    
+    restaurant_1.review = "From “pizza legend” Patsy Grimaldi, this “old-school” joint turns out “premium” pies via a coal oven (“it’s the char!”) in “casual” digs beneath the Brooklyn Bridge; since it “looks like everyone knows”, don’t be surprised to see lines “down the block.”"
+    restaurant_1.recommended_dishes = "No. 2 Pizza, White Pizza, Brookie Bridge Ice Cream Sandwich"
+    restaurant_1.insider_tip = "Good luck getting in without a wait now that the hordes in line at Grimaldi’s down the block have caught on. But in nice weather you can order your pie to-go and enjoy it – along with the killer harbor views – in nearby Brooklyn Bridge Park."
+
     restaurant_2 = self.new
     restaurant_2.name = "Emily"
     restaurant_2.location = "Clinton Hill"
     restaurant_2.price = "$$"
     restaurant_2.rating = "4.7 stars"
+    restaurant_2.review = "This “vibrant” pizzeria duo serves “out-of-this-world” Neapolitan pies with “perfectly charred” crusts and “inventive” toppings – though some swear the “killer” (but limited-supply) burger is the real “showstopper”; the Clinton Hill original specializes in rectangular pies, while the West Village outpost offers round too, but both spots are “tight”, so make a reservation or expect “absurdly long” waits."
+    restaurant_2.recommended_dishes = nil
+    restaurant_2.insider_tip = nil
+
 
     restaurant_3 = self.new
     restaurant_3.name = "Lucali"
     restaurant_3.location = "Carroll Gardens"
     restaurant_3.price = "$$"
     restaurant_3.rating = "4.7 stars"
+    restaurant_3.review = "It’s “hard to beat” the “ethereal” pizzas and “league-of-their-own” calzones that emerge from the brick oven at this cash-only Carroll Gardens joint; the BYO policy “makes it a great value”, but the trade-off is notoriously “insane waits.”"
+    restaurant_3.recommended_dishes = "Pizza Margherita, Calzone"
+    restaurant_3.insider_tip = "Waits here are pretty much always bring-a-book long, but at least it’s possible to call ahead and put your name on the list before setting out."
 
     restaurant_4 = self.new
     restaurant_4.name = "San Matteo Pizza Espresso Bar"
     restaurant_4.location = "Pizza Espresso Bar"
     restaurant_4.price = "$$"
     restaurant_4.rating = "4.6 stars"
+    restaurant_4.review = "Bite into the “first-rate”, “real-deal” Neapolitan pies or “unusual” panuozzi (sandwiches) at these “popular” Upper Eastsiders, and “you’re in Italy”; tables are “few” in the “tiny”, “basic” flagship, but the “expanded” East 80s outpost boasts a “lovely upstairs” space with a “fun vibe.”"
+    restaurant_4.recommended_dishes = nil
+    restaurant_4.insider_tip = nil
+
 
     restaurant_5 = self.new
     restaurant_5.name = "Roberta's"
     restaurant_5.location = "Bushwick"
     restaurant_5.price = "$$"
     restaurant_5.rating = "4.6 stars"
+    restaurant_5.review = "“Legendary” by now, this “hipster-chic” Bushwick destination is a “real standout” thanks to “expertly crafted” pizzas (try the “out-of-this-world” bee sting) and “equally amazing” Italian dishes, enhanced by homegrown produce; perpetual “crowds” make for “massive” waits, but once you're in it's “loads of fun.”"
+    restaurant_5.recommended_dishes = "Duck prosciutto, Famous Original Pizza, Rye & Gosling cocktail"
+    restaurant_5.insider_tip = "The wait is bound to be epically long, so while it away in the backyard bar – which is open year-round thanks to the tent and heating lamps."
 
     restaurant_6 = self.new
     restaurant_6.name = "Paulie Gee's"
     restaurant_6.location = "Greenpoint"
     restaurant_6.price = "$$"
     restaurant_6.rating = "4.6 stars"
+    restaurant_6.review = "The “crowds” prove the “hype” at this Greenpoint pizzeria where “seriously good” pies (especially the “addictive” Hellboy) are offered with “original” toppings varied enough “to please anyone”; the “dimly lit” digs feel especially “inviting” when Paulie “drops by to say hello.”"
+    restaurant_6.recommended_dishes = "Hellboy, Cherry Jones, Regina"
+    restaurant_6.insider_tip = "Takeout fans beware: it recently discontinued to-go orders, so prepare to dine-in (and most likely wait for a table)."
+
 
     restaurant_7 = self.new
     restaurant_7.name = "DI Fara Pizza"
     restaurant_7.location = "Midwood"
     restaurant_7.price = "$$"
     restaurant_7.rating = "4.6 stars"
+    restaurant_7.review = "Pizzaiolo “legend” Dom DeMarco is “a master plying his art” at this 1965 Midwood “institution”, where his “lovingly handmade” pies are “a must for the pizza addicted”; despite “unappealing” digs, “interminable waits” and service with “zero sense of urgency”, it’s “thronged” with those who urge “patience – it’s worth it.”"
+    restaurant_7.recommended_dishes = "Plain regular pie, Sicilian Square Pie, Di Fara Classic Pie"
+    restaurant_7.insider_tip = "If you want to avoid the long waits, you can call ahead and order a pie to-go."
+
 
     restaurant_8 = self.new
     restaurant_8.name = "Rubirosa Ristorante"
     restaurant_8.location = "NoLita"
     restaurant_8.price = "$$"
     restaurant_8.rating = "4.5 stars"
+    restaurant_8.review = "“Amazing” thin-crust pizzas and “to-die-for” meatballs are the fortes of this “charming” NoLita Italian done up in “rustic-chic” style; “cozy” turns “tight” during prime times, and it can be “hard to get a table”, but “cool” vibes and “reasonable” tabs compensate."
+    restaurant_8.recommended_dishes = "Classic Pizza, Baked Clams, Black and White Tagliatlle"
+    restaurant_8.insider_tip = "The pizzeria offers an entire gluten-free menu that includes pies made from GF dough."
+
 
     # puts "1. Juliana's - Dumbo - $$ - 4.7 stars"
     # puts "2. Emily - Brooklyn - Clinton Hill - $$ - 4.7 stars"
@@ -59,7 +88,7 @@ class NYCBest::Restaurant
     # puts "7. DI Fara Pizza - Midwood - $$ - 4.6 stars"
     # puts "8. Rubirosa Ristorante - NoLita - $$ - 4.5 stars"
 
-    [restaurant_1, restaurant_2, restaurant_3, restaurant_4, restaurant_5, restaurant_6, restaurant_7]
+    [restaurant_1, restaurant_2, restaurant_3, restaurant_4, restaurant_5, restaurant_6, restaurant_7, restaurant_8]
   end
 
 end
