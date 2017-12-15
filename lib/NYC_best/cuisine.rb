945d74acda2fc1089806ea75128103a478966f6f
Dani Schuhman <dani8439@aol.com>
2017-12-15 18:21:23 +0000

    classes

--- lib/NYC_best/cuisine.rb
+++ lib/NYC_best/cuisine.rb
@@ -1,5 +1,24 @@
+class InvalidType < StandardError; end
+
 class Cuisine
-  attr_accessor :restaurants, :name
+  attr_accessor :name
+  # attr_reader :restaurants
+
+  def initialize
+    @restaurants = []
+  end
+
+  def restaurants
+    @restaurants.dup.freeze
+  end
+
+  def add_restaurant(restaurant)
+    if !restaurant.is_a?(Restaurant)
+      raise InvalidType, "must be a Restaurant"
+    else
+      @restaurants << restaurant
+    end
+  end
 
   def first_restaurant_name
     # restaurants is an array
