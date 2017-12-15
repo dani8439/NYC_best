ab2959b5b7a8355917ada6eb76bf5bf3e92cbc2f
Dani Schuhman <dani8439@aol.com>
2017-12-15 21:23:44 +0000

    instances of restaurant

--- lib/NYC_best/cli.rb
+++ lib/NYC_best/cli.rb
@@ -4,36 +4,82 @@ class NYCBest::CLI
     puts "Welcome to NYC Best Pizza!"
     puts "----------------------------"
     sleep(1)
-    list_cuisines
+    # list_cuisines
+    list_restaurants
     menu
     goodbye
   end
 
-  def list_cuisines
-    cuisines = ["Brooklyn Pizza Delivery", "Manhattan Pizza Delivery", "NYC Pizza"]
-    # cuisines = ["Brooklyn Pizza", "Manhattan Pizza", "Steakhouse", "Indian", "Italian", "Mexican", "Seafood", "French", "Thai", "Barbecue", "Burger", "NYC Pizza", "Chinese", "American"]
-    cuisines.each.with_index(1) do |style, i|
-      puts "#{i}. #{style}"
-    end
+  def list_restaurants
+    # puts "1. Juliana's - Dumbo - $$ - 4.7 stars"
+    # puts "2. Emily - Brooklyn - Clinton Hill - $$ - 4.7 stars"
+    # puts "3. Lucali - Carroll Gardens - $$ - 4.7 stars"
+    # puts "4. San Matteo Pizza Espresso Bar - East 80s - $$ - 4.6 stars"
+    # puts "5. Roberta's - Bushwick - $$ - 4.6 stars"
+    # puts "6. Paulie Gee's - Greenpoint - $$ - 4.6 stars"
+    # puts "7. DI Fara Pizza - Midwood - $$ - 4.6 stars"
+    # puts "8. Rubirosa Ristorante - NoLita - $$ - 4.5 stars"
+    @restaurants = NYCBest::Restaurant.today
   end
 
   def menu
     input = nil
     while input != "exit"
-      puts "Please enter the number of the pizza you would like more info on, or type list to see choices again, or type exit:"
+      puts "Please enter the number of the restaurant you are interested in, or type list to see choices, or type exit."
       input = gets.strip.downcase
       case input
       when "1"
-        puts "(brooklyn_restaurants)"
+        puts "More info on Juliana's"
       when "2"
-        puts "(manhattan_restaurants)"
+        puts "More info on Emily's"
       when "3"
-        puts "(nyc_restaurants)"
+        puts "More info on Lucali"
+      when "4"
+        puts "More info on San Matteo Pizza Espresso Bar"
+      when "5"
+        puts "More info on Roberta's"
+      when "6"
+        puts "More info on Paulie Gee's"
+      when "7"
+        puts "More info on DI Fara Pizza"
+      when "8"
+        puts "More info on Rubirosa Ristorante "
       when "list"
-        list_cuisines
+        list_restaurants
+      else
+        puts "Not sure what you want, type list or exit."
       end
     end
   end
+
+
+  # def list_cuisines
+  #   cuisines = ["Brooklyn Pizza Delivery", "Manhattan Pizza Delivery", "NYC Pizza"]
+  #   # cuisines = ["Brooklyn Pizza", "Manhattan Pizza", "Steakhouse", "Indian", "Italian", "Mexican", "Seafood", "French", "Thai", "Barbecue", "Burger", "NYC Pizza", "Chinese", "American"]
+  #   cuisines.each.with_index(1) do |style, i|
+  #     puts "#{i}. #{style}"
+  #   end
+  # end
+  #
+  # def menu
+  #   input = nil
+  #   while input != "exit"
+  #     puts "Please enter the number of the pizza you would like more info on, or type list to see choices again, or type exit:"
+  #     input = gets.strip.downcase
+  #     case input
+  #     when "1"
+  #       puts "(brooklyn_restaurants)"
+  #     when "2"
+  #       puts "(manhattan_restaurants)"
+  #     when "3"
+  #       puts "(nyc_restaurants)"
+  #     when "list"
+  #       list_cuisines
+  #     else
+  #       puts "Not sure what you want, type list or exit."
+  #     end
+  #   end
+  # end
 
   def goodbye
     puts "See you when you're hungry!"
--- lib/NYC_best/restaurant.rb
+++ lib/NYC_best/restaurant.rb
@@ -1,5 +1,15 @@
-class Restaurant
+class NYCBest::Restaurant
   attr_accessor :name, :neighborhood, :price_level, :rating, :review, :recommended_dishes, :url
 
+  def self.today
+    puts "1. Juliana's - Dumbo - $$ - 4.7 stars"
+    puts "2. Emily - Brooklyn - Clinton Hill - $$ - 4.7 stars"
+    puts "3. Lucali - Carroll Gardens - $$ - 4.7 stars"
+    puts "4. San Matteo Pizza Espresso Bar - East 80s - $$ - 4.6 stars"
+    puts "5. Roberta's - Bushwick - $$ - 4.6 stars"
+    puts "6. Paulie Gee's - Greenpoint - $$ - 4.6 stars"
+    puts "7. DI Fara Pizza - Midwood - $$ - 4.6 stars"
+    puts "8. Rubirosa Ristorante - NoLita - $$ - 4.5 stars"
+  end
 
 end
